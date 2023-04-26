6ae70a4ecdffb1a8c1e0f74962e179a1b1cf6df8
Ashley Yaner <ashley.yaner@fractureid.com>
2023-04-26 13:08:55 -0600

    Implemented upload image and convert to grayscale. Day 19

--- bonus/bonus19.py
+++ bonus/bonus19.py
@@ -1,3 +1,33 @@
 import streamlit as st
+from PIL import Image
 
-st.camera_input("Camera")
+st.subheader("Color to Grayscale Converter")
+
+uploaded_image = st.file_uploader("Upload Image")
+
+# Add expander to start camera:
+with st.expander("Start Camera"):
+    # Start the camera. Take picture shows up on website
+    camera_image = st.camera_input("Camera")
+
+# If the picture was taken:
+if camera_image:
+    # Create pillow image instance
+    img = Image.open(camera_image)
+
+    # Convert pillow image to gray scale
+    gray_camera_img = img.convert("L")
+
+    # Render new gray scaled image
+    st.image(gray_camera_img)
+
+if uploaded_image:
+    # Create pillow image instance
+    img = Image.open(uploaded_image)
+
+    # Convert pillow image to gray scale
+    gray_uploaded_img = img.convert("L")
+
+    # Render new gray scaled image
+    st.image(gray_uploaded_img)
+
